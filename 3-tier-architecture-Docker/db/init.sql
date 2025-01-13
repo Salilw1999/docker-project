abfeab12d8d0f094cf1b443531fdc84111212481
Ubuntu <ubuntu@ip-172-31-47-113.ec2.internal>
2025-01-13 08:35:57 +0000

    first commit

---
+++ 3-tier-architecture-Docker/db/init.sql
@@ -0,0 +1,6 @@
+
+create database facebook;
+use facebook;
+create table users(id int , name varchar(20), email varchar(20), password varchar (200));
+insert into users values(1,'darshan','d@gmail.com','darshan@123'),(2,'niki','n@gmail.com','pass@123');
+
